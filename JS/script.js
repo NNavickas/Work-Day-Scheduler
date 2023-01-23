2f9ad22fd1fe841a0e834e599027be3ef6d73ef1
natalie navickas <natalienavickas@hotmail.com>
2023-01-23 14:20:05 +0000

    pseudocode

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,34 @@
+// get today's date (moment.js)
+
+
+//display date at top of page
+
+
+//create the blocks of time which are bookable 
+
+
+// local storage 
+
+
+//array of hour blocks to facilitate display
+
+
+        // ability to save the input 
+// button
+
+// event listener 
+
+// local storage
+
+
+        // ability to clear the input 
+// button
+
+// event listener 
+
+// local storage
+
+// 
+
+// change time block colour based on time compared to working day 
 
